f4fc6ddcb489d4e122b7e20434e027894b3bee66
keisuke <keisukes0714@gmail.com>
2023-08-09 23:46:23 +0900

    table function extension skeleton

--- include/msgpack-extension.hpp
+++ include/msgpack-extension.hpp
@@ -2,13 +2,22 @@
 #include <string>
 
 #include "duckdb.hpp"
+#include "duckdb/function/table_function.hpp"
 
 namespace duckdb {
 
-  class MsgpackExtension : public Extension {
-  public:
-    void Load(DuckDB &db) override;
-    std::string Name() override;
-  };
+struct MsgpackScanData : public TableFunctionData {};
+
+struct MsgpackScanInfo : public TableFunctionInfo {};
+
+struct MsgpackGlobalTableFunctionState : public GlobalTableFunctionState {};
+
+struct MsgpackLocalTableFunctionState : public LocalTableFunctionState {};
+
+class MsgpackExtension : public Extension {
+public:
+  void Load(DuckDB &db) override;
+  std::string Name() override;
+};
 
 } // namespace duckdb
--- msgpack-extension.cpp
+++ msgpack-extension.cpp
@@ -1,16 +1,31 @@
 #define DUCKDB_BUILD_LOADABLE_EXTENSION
 
 #include "msgpack-extension.hpp"
+#include "duckdb/common/multi_file_reader.hpp"
 
 namespace duckdb {
-  void MsgpackExtension::Load(DuckDB &db) {
+unique_ptr<FunctionData> ReadMsgpackBind(ClientContext &context,
+                                         TableFunctionBindInput &input,
+                                         vector<LogicalType> &return_types,
+                                         vector<string> &names) {}
 
-  }
-
-  std::string MsgpackExtension::Name() {
-    return "msgpack";
-  }
+static void ReadMsgpackFunction(ClientContext &context,
+                                TableFunctionInput &data_p, DataChunk &output) {
 }
+
+void MsgpackExtension::Load(DuckDB &db) {
+  auto &db_instance = *db.instance;
+
+  TableFunction table_function({LogicalType::VARCHAR}, ReadMsgpackFunction,
+                               ReadMsgpackBind,
+                               MsgpackGlobalTableFunctionState::Init,
+                               MsgpackLocalTableFunctionState::Init);
+  table_funciton.name = "read_msgpack";
+  MultiFileReader::CreateFunctionSet(table_function);
+}
+
+std::string MsgpackExtension::Name() { return "msgpack"; }
+} // namespace duckdb
 
 extern "C" {
 DUCKDB_EXTENSION_API void msgpack_init(duckdb::DatabaseInstance &db) {
