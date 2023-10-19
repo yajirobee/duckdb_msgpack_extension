b2d266cefe9fd7d89e544d9738152606eb4cad50
keisuke <keisukes0714@gmail.com>
2023-10-19 23:45:01 +0900

    transform string

--- src/msgpack_transform.cpp
+++ src/msgpack_transform.cpp
@@ -4,6 +4,30 @@
 
 #include "duckdb/common/types.hpp"
 
+namespace msgpack {
+MSGPACK_API_VERSION_NAMESPACE(MSGPACK_DEFAULT_API_NS) {
+  namespace adaptor {
+  template <> struct convert<duckdb::string_t> {
+    msgpack::object const &operator()(msgpack::object const &o,
+                                      duckdb::string_t &v) const {
+      switch (o.type) {
+      case msgpack::type::BIN:
+        v = duckdb::string_t(o.via.bin.ptr, o.via.bin.size);
+        break;
+      case msgpack::type::STR:
+        v = duckdb::string_t(o.via.str.ptr, o.via.str.size);
+        break;
+      default:
+        throw msgpack::type_error();
+        break;
+      }
+      return o;
+    }
+  };
+  } // namespace adaptor
+}
+} // namespace msgpack
+
 namespace duckdb {
 template <class T>
 static inline bool TransformVals(msgpack::object *values[], Vector &result,
@@ -13,6 +37,15 @@ static inline bool TransformVals(msgpack::object *values[], Vector &result,
   // TODO: handle type error
   for (idx_t row_idx = 0; row_idx < count; row_idx++) {
     data[row_idx] = values[row_idx]->as<T>();
+  }
+  return true;
+}
+
+static inline bool TransformStrings(msgpack::object *values[], Vector &result,
+                                    const idx_t count) {
+  auto data = FlatVector::GetData<string_t>(result);
+  for (idx_t row_idx = 0; row_idx < count; row_idx++) {
+    values[row_idx]->convert<string_t>(data[row_idx]);
   }
   return true;
 }
@@ -29,6 +62,8 @@ bool Transform(msgpack::object *values[], Vector &result, const idx_t count) {
     return TransformVals<bool>(values, result, count);
   case LogicalTypeId::INTEGER:
     return TransformVals<int32_t>(values, result, count);
+  case LogicalTypeId::VARCHAR:
+    return TransformStrings(values, result, count);
   default:
     throw NotImplementedException(
         "Cannot read a value of type %s from a json file",
