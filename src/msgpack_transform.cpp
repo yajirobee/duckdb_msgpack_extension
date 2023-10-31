4347d6bac0d1ef16b8ede9d0b169b55a586fbfb1
keisuke <keisukes0714@gmail.com>
2023-10-31 23:05:57 +0900

    trnsform scalar values

--- src/msgpack_transform.cpp
+++ src/msgpack_transform.cpp
@@ -36,18 +36,21 @@ static inline bool TransformVals(msgpack::object *values[], Vector &result,
   // TODO: set validity
   // TODO: handle type error
   for (idx_t row_idx = 0; row_idx < count; row_idx++) {
-    data[row_idx] = values[row_idx]->as<T>();
+    values[row_idx]->convert<T>(data[row_idx]);
   }
   return true;
 }
 
-static inline bool TransformStrings(msgpack::object *values[], Vector &result,
-                                    const idx_t count) {
-  auto data = FlatVector::GetData<string_t>(result);
+static bool TransformFromString(msgpack::object *values[], Vector &result,
+                                const idx_t count) {
+  Vector string_vector(LogicalTypeId::VARCHAR, count);
+
+  auto data = FlatVector::GetData<string_t>(string_vector);
   for (idx_t row_idx = 0; row_idx < count; row_idx++) {
     values[row_idx]->convert<string_t>(data[row_idx]);
   }
-  return true;
+
+  return VectorOperations::DefaultTryCast(string_vector, result, count, nullptr, true);
 }
 
 bool Transform(msgpack::object *values[], Vector &result, const idx_t count) {
@@ -60,13 +63,46 @@ bool Transform(msgpack::object *values[], Vector &result, const idx_t count) {
     return true;
   case LogicalTypeId::BOOLEAN:
     return TransformVals<bool>(values, result, count);
+  case LogicalTypeId::TINYINT:
+    return TransformVals<int8_t>(values, result, count);
+  case LogicalTypeId::SMALLINT:
+    return TransformVals<int16_t>(values, result, count);
   case LogicalTypeId::INTEGER:
     return TransformVals<int32_t>(values, result, count);
+  case LogicalTypeId::BIGINT:
+    return TransformVals<int64_t>(values, result, count);
+  case LogicalTypeId::UTINYINT:
+    return TransformVals<uint8_t>(values, result, count);
+  case LogicalTypeId::USMALLINT:
+    return TransformVals<uint16_t>(values, result, count);
+  case LogicalTypeId::UINTEGER:
+    return TransformVals<uint32_t>(values, result, count);
+  case LogicalTypeId::UBIGINT:
+    return TransformVals<uint64_t>(values, result, count);
+  case LogicalTypeId::FLOAT:
+    return TransformVals<float>(values, result, count);
+  case LogicalTypeId::DOUBLE:
+    return TransformVals<double>(values, result, count);
+  case LogicalTypeId::AGGREGATE_STATE:
+  case LogicalTypeId::ENUM:
+  case LogicalTypeId::DATE:
+  case LogicalTypeId::INTERVAL:
+  case LogicalTypeId::TIME:
+  case LogicalTypeId::TIME_TZ:
+  case LogicalTypeId::TIMESTAMP:
+  case LogicalTypeId::TIMESTAMP_TZ:
+  case LogicalTypeId::TIMESTAMP_NS:
+  case LogicalTypeId::TIMESTAMP_MS:
+  case LogicalTypeId::TIMESTAMP_SEC:
+  case LogicalTypeId::UUID:
+    return TransformFromString(values, result, count);
+  case LogicalTypeId::CHAR:
+    return TransformVals<string_t>(values, result, count);
   case LogicalTypeId::VARCHAR:
-    return TransformStrings(values, result, count);
+    return TransformVals<string_t>(values, result, count);
   default:
     throw NotImplementedException(
-        "Cannot read a value of type %s from a json file",
+        "Cannot read a value of type %s from a msgpack file",
         result_type.ToString());
   }
 }
